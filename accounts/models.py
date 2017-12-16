a03624276dc9deae0b06ff4e0147487919f3ff23
Zachary-Jackson <jackson.zachary.r@gmail.com>
2017-12-16 14:16:18 -0500

    Added the user's profile model
    The model for a user's profile information has been added. The requirments.txt has been updated to allow the user to upload an avatar image.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    '''This is the profile model for a User.'''
+    user = models.OneToOneField(User, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(max_length=1000, blank=True)
+    avatar = models.ImageField(blank=True)
+    hobby = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.user)
