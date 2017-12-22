09b827b5e0a9f86a57dd031bd37024d4fc30605b
Zachary-Jackson <jackson.zachary.r@gmail.com>
2017-12-22 17:48:21 -0500

    User Profile V1.1
    The password validation has been updated to include an integer checker.

--- accounts/validators.py
+++ accounts/validators.py
@@ -8,25 +8,30 @@ class CustomPasswordValidator(object):
         '''This checks to see if there is an int, upper and lowercase
         letter, and that there are special characters.'''
         special_characters = "[~\!@#\$%\^&\*\(\)_\+{}\":;'\[\]]"
-        # This checks to see is the password has upper and lowercase letters.
+        # This checks to see is the password has upper and lowercase letters,
+        # an integer, and a special character.
         found_upper = False
         found_lower = False
         found_special = False
+        found_int = False
         for letter in password:
             if letter in special_characters:
                 found_special = True
             else:
-                if letter.isupper():
+                if letter.isdigit():
+                    found_int = True
+                elif letter.isupper():
                     found_upper = True
                 else:
                     found_lower = True
 
         # Checks to see if any letter is not found.
-        if not found_lower or not found_upper or not found_special:
+        if (not found_lower or not found_upper
+                or not found_special or not found_int):
             raise ValidationError('Password must contain an upper and ' +
                                   'lowercase letter as well as ' +
-                                  ' a special character.')
+                                  ' a special character and integer.')
 
     def get_help_text(self):
         return ('Password must contain an upper and lowercase letter'
-                ' as well as a special character.')
+                ' as well as a special character and integer.')
